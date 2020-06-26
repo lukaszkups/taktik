ed47bc2436c4791b1d969853139fe27256bdf01f
lukaszkups <mrmnmly@gmail.com>
2020-06-26 19:58:58 +0200

    Thinking about level data structure.

--- src/levels/level_1.js
+++ src/levels/level_1.js
@@ -8,5 +8,13 @@ export default {
     'sand', 'sand', 'sand', 'sand', 'sand', 'sand', 'forest', 'sand',
     'sand', 'sand', 'forest', 'sand', 'sand', 'sand', 'sand', 'mountains',
     'sand', 'sand', 'sand', 'forest', 'sand', 'mountains', 'mountains', 'mountains'
+  ],
+  incoming: [
+    { tile: 16, unit: 'MadCat', turn: 0, action: {} },
+    { tile: 37, unit: 'Tank', turn: 0, action: {} },
+    { tile: 38, unit: 'Tank', turn: 1, action: {} }
+  ],
+  enemy: [
+    { tile: 28, unit: 'Tank', action: { move: 1, attack: 1 } } // 0 - false, 1 - true
   ]
 }
